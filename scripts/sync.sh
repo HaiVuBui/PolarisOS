5907ed79cc944944bd513c98d556dd6bc232faf4
HaiVuBui <buivuhai1105@gmail.com>
2025-11-30 19:08:46 +0700

    (feat) htop: percent_mem -> M_resident

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,7 @@
 rm -rf dotfiles/settings.json
+rm -rf dotfiles/htop/
 rm -rf wallpapres/Selected
 
 cp ~/.config/Code/User/settings.json dotfiles/
+cp -r ~/.config/htop dotfiles/
 cp ~/Wallpapers/* wallpapers/Selected
