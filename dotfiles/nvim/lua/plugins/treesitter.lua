e4a7ed3261fcc0d95310800811bbdf979ed1747f
HaiVuBui <buivuhai1105@gmail.com>
2025-12-20 21:04:19 +0700

    (fix) nvim: fix treesitter error, point to branch master

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master", -- pin to the stable API (new default branch is an incompatible rewrite)
   build = ":TSUpdate",
   opts = {
     ensure_installed = {
