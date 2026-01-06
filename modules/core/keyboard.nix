05f86a751cb1f80847e824a2076bb5b794d76523
HaiVuBui <buivuhai1105@gmail.com>
2026-01-06 22:36:51 +0700

    (feat) add fcitx5 for vietnamese typing

--- modules/core/keyboard.nix
+++ modules/core/keyboard.nix
@@ -1,8 +1,24 @@
-{ userConfig, pkgs, ... }:
+{ pkgs, ... }: # Ensure this line has 'pkgs' inside the { }
 
 {
-  services.xserver = {
-  xkb.options = "caps:backspace";
-};
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-bamboo
+      fcitx5-gtk
+    ];
+  };
 
+  # We use 'environment.variables' directly to match the system module exactly.
+  # We use 'pkgs.lib.mkForce' to guarantee this overrides the default "fcitx".
+  environment.variables = {
+    GTK_IM_MODULE = pkgs.lib.mkForce "";
+  };
+
+  # These can stay in sessionVariables or variables, doesn't matter much.
+  environment.sessionVariables = {
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
 }
