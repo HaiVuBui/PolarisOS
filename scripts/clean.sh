44ddf1669da9dc332d090738384f5682133cd83a
HaiVuBui <buivuhai1105@gmail.com>
2026-01-15 01:21:53 +0700

    (feat) add clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,7 +2,11 @@
 nix store optimise
 nh clean all
 sudo mount -o remount,rw /nix/store
+echo 'defrag /nix'
 sudo btrfs filesystem defragment -r -czstd /nix/
+echo 'defrag /home'
 sudo btrfs filesystem defragment -r -czstd /home/
+echo 'defrag /'
 sudo btrfs filesystem defragment -r -czstd /
+echo 'cleanning done'
 
