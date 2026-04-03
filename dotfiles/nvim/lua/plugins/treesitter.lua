29d94850b4d6f4632657a81d36c7c328b962fceb
HaiVuBui <buivuhai1105@gmail.com>
2026-04-03 23:18:53 +0700

    fix nvim config

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,6 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master", -- pin to the stable API (new default branch is an incompatible rewrite)
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "c",
-      "cpp",
-      "python",
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-      "elixir",
-      "heex",
-      "javascript",
-      "html",
-      "markdown",
-      "markdown_inline",
-      "latex",
-      "rust"
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+return
+{
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate'
 }
