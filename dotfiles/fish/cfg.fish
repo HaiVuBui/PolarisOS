cf9813ec31fa6fde53d718c3a74738a85f27f7aa
HaiVuBui <buivuhai1105@gmail.com>
2026-04-11 18:43:34 +0700

    alias for note in vscode

--- dotfiles/fish/cfg.fish
+++ dotfiles/fish/cfg.fish
@@ -29,6 +29,7 @@ if status is-interactive
     abbr --add lt 'eza --icons=always --tree'
     
     # Tools
+    abbr --add ns "code ~/GrandArchive/"
     abbr --add y yazi
     abbr --add m rmpc
     abbr --add lzg lazygit
