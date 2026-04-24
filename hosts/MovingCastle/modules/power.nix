de0dee61377d558122a99d8b7c4011cf14a6a210
HaiVuBui <buivuhai1105@gmail.com>
2026-04-24 13:23:49 +0700

    new powerprofile for MovingCastle

--- hosts/MovingCastle/modules/power.nix
+++ hosts/MovingCastle/modules/power.nix
@@ -6,31 +6,31 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-      CPU_MIN_PERF_ON_AC = 0;
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+      CPU_MIN_PERF_ON_AC = 100;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 60;
+      CPU_MAX_PERF_ON_BAT = 80;
       CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
+      CPU_BOOST_ON_BAT = 1;
       SCHED_POWERSAVE_ON_AC = 0;
-      SCHED_POWERSAVE_ON_BAT = 1;
+      SCHED_POWERSAVE_ON_BAT = 0;
 
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "balanced";
 
       DISK_IDLE_SECS_ON_AC = 0;
       DISK_IDLE_SECS_ON_BAT = 2;
-      DISK_APM_LEVEL_ON_AC = "254 254";
+      DISK_APM_LEVEL_ON_AC = "255 255";
       DISK_APM_LEVEL_ON_BAT = "128 128";
-      SATA_LINKPWR_ON_AC = "med_power_with_dipm";
+      SATA_LINKPWR_ON_AC = "max_performance";
       SATA_LINKPWR_ON_BAT = "med_power_with_dipm";
-      AHCI_RUNTIME_PM_ON_AC = "on";
+      AHCI_RUNTIME_PM_ON_AC = "off";
       AHCI_RUNTIME_PM_ON_BAT = "auto";
 
-      PCIE_ASPM_ON_AC = "default";
+      PCIE_ASPM_ON_AC = "performance";
       PCIE_ASPM_ON_BAT = "powersupersave";
-      RUNTIME_PM_ON_AC = "on";
+      RUNTIME_PM_ON_AC = "off";
       RUNTIME_PM_ON_BAT = "auto";
 
       USB_AUTOSUSPEND = 1;
@@ -43,7 +43,8 @@
       SOUND_POWER_SAVE_ON_BAT = 1;
       SOUND_POWER_SAVE_CONTROLLER = "Y";
 
-      STOP_CHARGE_THRESH_BAT0 = 1;
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
     };
   };
 
