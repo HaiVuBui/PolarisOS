c4791e16cc6ec11999b4723e02059276f9cbbbb3
HaiVuBui <buivuhai1105@gmail.com>
2026-05-09 18:00:13 +0700

    new fastfetch config

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -2,11 +2,6 @@
 
 {
   gtk = {
-    # enable = true;
-    # theme = {
-    #   name = "Everforest-Light";
-    #   package = pkgs.everforest-gtk-theme;
-    # };
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
--- modules/home/qt.nix
+++ modules/home/qt.nix
@@ -1,7 +1,7 @@
-{ lib, ... }: {
+{ lib, ... }:
+{
   qt = {
     enable = true;
     platformTheme.name = lib.mkForce "qtct";
   };
 }
-
