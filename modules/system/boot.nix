7f6f47a881453ccb8c6e4a929474629ac13324d7
HaiVuBui <buivuhai1105@gmail.com>
2026-05-09 11:20:36 +0700

    disable EFI shell

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -15,29 +15,5 @@
       editor = true;
     };
 
-    # plymouth.enable = true;
-    # plymouth.theme = "stylix";
-
-    kernelParams = [
-      "quiet"
-      "splash"
-      "loglevel=0"
-      "rd.systemd.show_status=auto"
-      "systemd.show_status=false"
-      "udev.log_priority=0"
-      "workqueue.power_efficient=0"
-    ];
-
-    consoleLogLevel = 3;
-  };
-
-  # --- THE "BIOS BYPASS" MAGIC ---
-  # This script runs every time you rebuild.
-  # It forces the EFI Shell binary into your boot partition.
-  system.activationScripts.installEfiShell = {
-    text = ''
-      # Copy the shell binary to the specific name systemd-boot looks for
-      cp -f ${pkgs.edk2-uefi-shell}/shell.efi /boot/shellx64.efi
-    '';
   };
 }
