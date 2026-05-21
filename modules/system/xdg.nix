c7e5d696ab651b42890f3d074a6886fb778ef3ae
HaiVuBui <buivuhai1105@gmail.com>
2026-05-22 01:18:17 +0700

    make helium default

--- modules/system/xdg.nix
+++ modules/system/xdg.nix
@@ -6,11 +6,11 @@
   xdg.mime.enable = true;
 
   xdg.mime.defaultApplications = {
-    "text/html" = "brave-browser.desktop";
-    "x-scheme-handler/http" = "brave-browser.desktop";
-    "x-scheme-handler/https" = "brave-browser.desktop";
-    "x-scheme-handler/about" = "brave-browser.desktop";
-    "x-scheme-handler/unknown" = "brave-browser.desktop";
+    "text/html" = "helium.desktop";
+    "x-scheme-handler/http" = "helium.desktop";
+    "x-scheme-handler/https" = "helium.desktop";
+    "x-scheme-handler/about" = "helium.desktop";
+    "x-scheme-handler/unknown" = "helium.desktop";
   };
 }
 
