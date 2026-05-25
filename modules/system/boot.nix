a848a3de45a78971618bc1c39b591cca3cffb3ef
HaiVuBui <buivuhai1105@gmail.com>
2026-05-25 18:00:56 +0700

    clean boot config

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -11,21 +11,11 @@
     initrd = {
       systemd.enable = true;
       supportedFilesystems = [ "btrfs" ];
-      systemd.emergencyAccess = true;
-      availableKernelModules = [
-        "nvme"
-        "xhci_pci"
-        "ahci"
-        "usb_storage"
-        "sd_mod"
-        "btrfs"
-      ];
     };
 
-    loader.systemd-boot = {
-      enable = true;
+    loader = {
+      timeout = 3;
+      systemd-boot.enable = true;
     };
-    kernelParams = [ "rd.systemd.debug_shell=1" ];
-
   };
 }
