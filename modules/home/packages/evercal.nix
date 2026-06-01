8349c2310c64cb903ea7ffd0b8b2b486868a43a5
HaiVuBui <buivuhai1105@gmail.com>
2026-06-01 21:32:16 +0700

    evercal new features

--- modules/home/packages/evercal.nix
+++ modules/home/packages/evercal.nix
@@ -4,8 +4,8 @@ let
   src = pkgs.fetchFromGitHub {
     owner = "HaiVuBui";
     repo = "EverCal";
-    rev = "31d89db53f63";
-    sha256 = "sha256-lYarES3jGgnh81D/A9Fbo680aj4haaQxbbHPuE3DLvs";
+    rev = "33c91b20d3c5";
+    sha256 = "sha256-KoUqEaZA639BdDwbjBod6FLneDGbiTg5QZAZV7/hZ68";
   };
 in
 pkgs.flutter.buildFlutterApplication rec {
