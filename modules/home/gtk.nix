e5023de8000d431248912d09ae82bf90162b3c08
HaiVuBui <buivuhai1105@gmail.com>
2026-06-05 23:52:10 +0700

    cat latte gtk and qt

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -1,11 +1,16 @@
 { pkgs, config, ... }:
 
 {
+  dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-light";
+
   gtk = {
     enable = true;
     theme = {
-      name = "rose-pine-dawn";
-      package = pkgs.rose-pine-gtk-theme;
+      name = "catppuccin-latte-blue-standard";
+      package = pkgs.catppuccin-gtk.override {
+        variant = "latte";
+        accents = [ "blue" ];
+      };
     };
     iconTheme = {
       name = "Papirus-Dark";
@@ -14,8 +19,6 @@
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 0;
     };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 0;
-    };
+    gtk4.theme = config.gtk.theme;
   };
 }
--- modules/home/qt.nix
+++ modules/home/qt.nix
@@ -1,30 +1,23 @@
-{ lib, ... }:
+{ pkgs, ... }:
+let
+  qtctConf = ver: ''
+    [Appearance]
+    color_scheme_path=${pkgs.catppuccin-qt5ct}/share/${ver}/colors/catppuccin-latte-blue.conf
+    icon_theme=Papirus-Dark
+    standard_dialogs=default
+    style=Fusion
+
+    [Fonts]
+    fixed="JetBrainsMono Nerd Font Mono,12"
+    general="Noto Serif,12"
+  '';
+in
 {
   qt = {
     enable = true;
-    platformTheme.name = lib.mkForce "qtct";
+    platformTheme.name = "qtct";
   };
 
-  xdg.configFile."qt5ct/qt5ct.conf".text = ''
-    [Appearance]
-    custom_palette=true
-    icon_theme=Papirus-Dark
-    standard_dialogs=default
-    style=kvantum
-
-    [Fonts]
-    fixed="JetBrainsMono Nerd Font Mono,12"
-    general="Noto Serif,12"
-  '';
-
-  xdg.configFile."qt6ct/qt6ct.conf".text = ''
-    [Appearance]
-    icon_theme=Papirus-Dark
-    standard_dialogs=default
-    style=kvantum
-
-    [Fonts]
-    fixed="JetBrainsMono Nerd Font Mono,12"
-    general="Noto Serif,12"
-  '';
+  xdg.configFile."qt5ct/qt5ct.conf".text = qtctConf "qt5ct";
+  xdg.configFile."qt6ct/qt6ct.conf".text = qtctConf "qt6ct";
 }
