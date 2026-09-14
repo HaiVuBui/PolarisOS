ad82b2769a5841a835d0aefccff8da6e008628fd
HaiVuBui <buivuhai1105@gmail.com>
2026-09-15 00:54:30 +0700

    remove old symlinks

--- modules/home/symlinks.nix
+++ modules/home/symlinks.nix
@@ -3,11 +3,11 @@
 {
   # Out-of-store symlinks: the link lives in $HOME, the target stays mutable.
   home.file = {
-    "GrandArchive/Textbooks".source =
-      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Textbooks";
-
-    "GrandArchive/Papers".source =
-      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Papers";
+    # "GrandArchive/Textbooks".source =
+    #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Textbooks";
+    #
+    # "GrandArchive/Papers".source =
+    #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Papers";
 
     "Wallpapers".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/PolarisOS/wallpapers";
